5d9d21f1b5a326febc0aa01c16fdc49c7d748689
Newo Ether <newoether2000@gmail.com>
2026-06-01 12:39:51 +0800

    release: v1.0.9 (versionCode 9)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.newoether.agora"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.8"
+        versionCode = 9
+        versionName = "1.0.9"
 
 
         ndk {
